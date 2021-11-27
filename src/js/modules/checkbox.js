c7832bb4480a8be5da09cbb9194f95c626e76e77
Ivan Fedak <joing3497@gmail.com>
2021-11-27 10:43:28 +0200

    start img

--- src/js/modules/checkbox.js
+++ src/js/modules/checkbox.js
@@ -2,24 +2,24 @@ const checkBox = ()=>{
     const checkBox = document.querySelectorAll('.buy-component__checkbox');
 
     checkBox.forEach(item=>{
-        const input = item.querySelector('input')
+        const input = item.querySelector('input');
         if(input.checked == true){
-            item.classList.add('_active')
+            item.classList.add('_active');
         }
 
         item.addEventListener('click',function(e){
             if(item.classList.contains('_active')){
-                input.checked = false
-                console.log(1)
+                input.checked = false;
+                console.log(1);
             }
             else{
-                input.checked = true
-                console.log(2)
+                input.checked = true;
+                console.log(2);
             }
-            item.classList.toggle('_active')
-        })
+            item.classList.toggle('_active');
+        });
         
-    })
+    });
 
 
     const checkBox2 = document.querySelectorAll('.where-component__checkbox');
